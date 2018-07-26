a111784621a2efbcffaf74bb5f42893b6b472b32
kaifchen <arm9home@126.com>
2018-07-26 22:31:48 +0800

    C#源代码
    V1.0

---
+++ Mqtt-client/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Mqtt_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendTxtBox = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.receiveTxtBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // sendTxtBox
+            // 
+            this.sendTxtBox.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sendTxtBox.Location = new System.Drawing.Point(13, 341);
+            this.sendTxtBox.Multiline = true;
+            this.sendTxtBox.Name = "sendTxtBox";
+            this.sendTxtBox.Size = new System.Drawing.Size(532, 97);
+            this.sendTxtBox.TabIndex = 1;
+            this.sendTxtBox.Text = "这是一个Mqtt客户端测试程序！";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(592, 332);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(164, 50);
+            this.sendBtn.TabIndex = 2;
+            this.sendBtn.Text = "发布消息";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(592, 389);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(164, 50);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "关闭";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // receiveTxtBox
+            // 
+            this.receiveTxtBox.BackColor = System.Drawing.SystemColors.Window;
+            this.receiveTxtBox.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.receiveTxtBox.Location = new System.Drawing.Point(13, 13);
+            this.receiveTxtBox.Name = "receiveTxtBox";
+            this.receiveTxtBox.ReadOnly = true;
+            this.receiveTxtBox.Size = new System.Drawing.Size(775, 309);
+            this.receiveTxtBox.TabIndex = 3;
+            this.receiveTxtBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.receiveTxtBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.sendTxtBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mqtt客户端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //void client_mqttmsgpublishreceived(object sender, mqttmsgpublisheventargs e)
+        //{
+        //    //处理接收到的消息
+        //    string msg = system.text.encoding.default.getstring(e.message);
+        //    receivetxtbox.appendtext("收到消息：" + msg + "\r\n");
+        //}
+        private System.Windows.Forms.TextBox sendTxtBox;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.RichTextBox receiveTxtBox;
+    }
+}
+
---
+++ Mqtt-client/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Mqtt_Client
+{
+    public partial class Form1 : Form
+    {
+        string MQTT_ADDRESS = "127.0.0.1";
+        int MQTT_PORT = 61613;
+        string MQTT_TOPIC = "/home/temperature";//订阅的主题"/home/temperature" 
+        private MqttClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //receiveTxtBox.ForeColor = Color.Red;
+
+            /*
+             * 必须加入如下语句，否则会出现如下错误信息：
+             * System.InvalidOperationException:“线程间操作无效: 从不是创建控件“receiveTxtBox”的线程访问它。”
+             */
+            CheckForIllegalCrossThreadCalls = false;
+
+            string tradeTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            receiveTxtBox.ForeColor = Color.Red;
+            receiveTxtBox.AppendText(tradeTime + " 正在连接服务器……\r\n");
+            //创建客户端实例
+            client = new MqttClient(MQTT_ADDRESS, MQTT_PORT, false, null, null, MqttSslProtocols.None);
+
+            // 注册消息接收处理事件，还可以注册消息订阅成功、取消订阅成功、与服务器断开等事件处理函数
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            //生成客户端ID并连接服务器
+            string clientId = System.Guid.NewGuid().ToString(); //Guid.NewGuid().ToString();
+            client.Connect(clientId, "user", "123456");
+
+            // 订阅主题"/home/temperature" 消息质量为 0 
+            //client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.Subscribe(new string[] { MQTT_TOPIC }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+
+            tradeTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            if (client.IsConnected)
+            {
+                receiveTxtBox.SelectionColor = Color.Blue;
+                receiveTxtBox.AppendText(tradeTime + " 成功连接服务器。\r\n");
+            }
+            else
+                receiveTxtBox.AppendText(tradeTime + " 连接服务器失败！\r\n");
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            // 发布消息到主题 "/home/temperature" 消息质量为 2,不保留 
+            // client.Publish(MQTT_TOPIC, Encoding.UTF8.GetBytes("Hello"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);//发布的是固定消息“Hello”
+            client.Publish(MQTT_TOPIC, Encoding.UTF8.GetBytes(sendTxtBox.Text.Trim()), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            //获取当前时间
+            string tradeTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+
+            //处理接收到的消息
+            string msg = System.Text.Encoding.UTF8.GetString(e.Message);
+            //receiveTxtBox.AppendText(tradeTime + " 收到消息：" + msg + "\r\n");//常规处理接收到的消息
+
+            //以下增加了接收到的消息内容颜色不同的功能
+            receiveTxtBox.SelectionColor = Color.Red;
+            receiveTxtBox.SelectedText = tradeTime+ " 收到消息：";
+            receiveTxtBox.SelectionColor = Color.DarkCyan;
+            receiveTxtBox.AppendText(msg + "\r\n");
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //receiveTxtBox.AppendText("正在退出系统，断开服务器的连接！\r\n");
+            client.Disconnect();
+        }
+    }
+}
